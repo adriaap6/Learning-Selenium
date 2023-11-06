ba0510c76f38e773ae2abf078a371e1b2ad24f08
adriaap6 <adriatisnawatiaprilia@gmail.com>
2023-11-06 21:18:15 +0700

    screenshot

---
+++ screenshot.py
@@ -0,0 +1,13 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+
+options = webdriver.ChromeOptions()
+options.add_experimental_option('excludeSwitches', ['enable-logging'])
+options.headless = True
+options.add_argument('--window-size=1920,1080')
+
+driver = webdriver.Chrome(options=options)
+
+driver.get("http://demoqa.com")
+print(driver.title)
+driver.get_screenshot_as_file("screenshot2.png")
